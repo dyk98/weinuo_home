35ece50cf97b05de1373e7e0d6d5b9363d2858d1
Leu552 <1055112835@qq.com>
2019-01-06 15:24:50 +0800

    'feat整理了industry组件‘

--- phone_front_end/src/components/industry.vue
+++ phone_front_end/src/components/industry.vue
@@ -3,93 +3,7 @@
         <div class="section_title">
             <h3>行业</h3>
         </div>
-        <div class="container">
-        <ul class="myworks-list" width="100%">
 
-            <li class="mlli mlli1">
-                <a class="mwimg mwimg1" href="/works_show.aspx?id=180"><span class="icon icon-bag"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=180">快消</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=180">快消行业是指消费频率高，使用时限短，拥有广泛的消费群体，对于消费的便利性要求很高的商品销售行业。快消行业的销售渠道种类多而复杂，传统业态和新兴业态等多种渠道并存。随着世界经济的融合和中国经济的进一步发展, 互联网对快消行业的影响越来越大。进口消费品正一步步扩…</a></div>
-                </div>
-            </li>
-
-            <li class="mlli mlli2">
-                <a class="mwimg mwimg2" href="/works_show.aspx?id=183"><span class="icon icon-workspace"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=183">家具</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=183">中国目前家具零售业无外乎有四个模式：第一种模式：经销商开的独立店，青田、创典居都属于独立店，他们经营家具都有十几年的历史，在一个区域里都能开自己成型的店。第二种模式：大卖场，像红星、月星、罗浮宫的这种大卖场。招商模式的可以称为店中店。第三种模式：工厂直接开…</a></div>
-                </div>
-            </li>
-
-            <li class="mlli mlli3">
-                <a class="mwimg mwimg3" href="/works_show.aspx?id=182"><span class="icon icon-shirt"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=182">服装</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=182">中国是十三亿人口的大国，是全世界最大的服装消费国和生产国。近几年中国的服装业有着较大的发展，服装业的发展大大推动了中国国民经济的发展，2005年纺织服装的总产值约占全国总产值的十分之一，并已连续五年出口创汇顺差第一，服装产业一直为中国出口创汇作出了巨大的贡献。…</a></div>
-                </div>
-            </li>
-
-            <li class="mlli mlli4">
-                <a class="mwimg mwimg4" href="/works_show.aspx?id=185"><span class="icon icon-leaf"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=185">空调</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=185">随着技术的创新与发展，人们的生活水平也跟着提高，现在无论是家庭还是商场都离不开空调，空调行业也跟着市场需求经历了多年的高速发展。目前家用机市场的产品型号越来越多，国内品牌的空调也陆续走向国际化。十几年来，品牌企业通过不断的调整，厂商对渠道的绝对控制，形成整…</a></div>
-                </div>
-            </li>
-
-            <li class="mlli mlli5">
-                <a class="mwimg mwimg5" href="/works_show.aspx?id=186"><span class="icon icon-sister"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=186">医疗</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=186">计划经济体制下，医疗卫生体系定位明确，中国的医疗卫生创造了一系列辉煌，在医疗服务、预防保健等各个方面都取得了很大的成就。农村和城镇的医疗服务也在这时全面展开，医疗服务的可及性大大增强。由于社会经济发展和综合国力的影响以及“政事一体化”的管理，中国在医疗技术…</a></div>
-                </div>
-            </li>
-
-            <li class="mlli mlli6">
-                <a class="mwimg mwimg6" href="/works_show.aspx?id=181"><span class="icon icon-cart-full"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=181">连锁</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=181">我国自20世纪90年代初开始发展连锁经营以来，由少到多，从小到大，逐步发展壮大，在零售市场的主导地位不断提高，有力地推动了我国商业体制的改革和流通产业的现代化。由于经营方式与组织结构所独具的优势，使其很快成为现代流通业的发展主流和商业领域最具活力的业态，充分显…</a></div>
-                </div>
-            </li>
-
-            <li class="mlli mlli7">
-                <a class="mwimg mwimg7" href="/works_show.aspx?id=184"><span class="icon icon-plane"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=184">航空</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=184">中国是人口大国，旺盛的需求支撑中国航空业客座率的不断，提升，从1998年至今，中国航空业客座率从60%持续提升至80%，寡头垄断和市场化的竞争格局并存。经历近30年的扩展和整合，目前国航、东航、南航和海航四大集团合计控制运力约85%，考虑枢纽机场的时刻瓶颈和主基地航空公司…</a></div>
-                </div>
-            </li>
-
-            <li class="mlli mlli8">
-                <a class="mwimg mwimg8" href="/works_show.aspx?id=187"><span class="icon icon-bus1"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=187">交通</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=187">交通，是一个城市的命脉。交通是城乡一体的大纽带，是城市发展的大动脉，作为基础性、先导性、服务性行业，在经济社会发展中的地位和作用日益凸显。交通是商品交换的先决条件。随着交通的改善，人类的物质生产逐步从自给自足的方式，过渡到分工交换的方式。物质产品的分工交换…</a></div>
-                </div>
-            </li>
-
-            <li class="mlli mlli9">
-                <a class="mwimg mwimg9" href="/works_show.aspx?id=188"><span class="icon icon-balance"></span></a>
-
-                <div class="mwcon">
-                    <h2 class="mwname"><a href="/works_show.aspx?id=188">政府</a></h2>
-                    <div class="mwdiscribe"><a href="/works_show.aspx?id=188">电子政务是经济与社会信息化的先决条件，是指公共管理部门全面运用现代信息技术、通信技术和管理理论，将管理和服务通过网络技术进行集成，实现组织结构和业务流程的优化重组，超越时间、空间与部门分隔的限制，全方位地实施对社会的管理职能，是一种全新的管理方式，向社会提…</a></div>
-                </div>
-            </li>
-
-            <div class="clearfix"></div>
-        </ul>
-        </div>
     </div>
 </template>
 
@@ -128,12 +42,5 @@
     .section_title {
         height: 3em;
         text-align: center;
-    }
-    .container {
-        max-width: 117em;
-        padding-right: 1.5em;
-        padding-left: 1.5em;
-        margin-right: auto;
-        margin-left: auto;
     }
 </style>
