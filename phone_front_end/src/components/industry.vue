71ce30f30f7d7236e1e35f55f6aee5d27a149639
Leu552 <1055112835@qq.com>
2019-01-06 20:05:45 +0800

    feat更新industry组件内容

--- phone_front_end/src/components/industry.vue
+++ phone_front_end/src/components/industry.vue
@@ -1,10 +1,160 @@
 <template>
-    <div class="industry_area">
+    <!--行业-->
+    <div class="industry_area" style="height: auto">
         <div class="section_title">
             <h3>行业</h3>
         </div>
+        <ul>
 
+            <li class="pagelist wlist1">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=180"><span class="icon icon-beer"></span></a>
+                </div>
+
+                <h2 class="plname wlname"><a href="/works_show.aspx?id=180">农业</a></h2>
+                <div class="plcon wlcon"><a href="/works_show.aspx?id=180">
+                    “互联网+农业”是一种生产方式、产业模式与经营手段的创新，通过便利化、实时化、物联化、智能化等手段，对农业的生产、经营、管理、服务等农业产业链环节产生了深远影响，为农业现代化发展提供了新动力。以“互联网+农业”为驱动，有助于发展智慧农业、精细农业、高效农业、绿色农业，提高农业质量效益和竞争力，实现由传统农业向现代农业转型。…
+                </a>
+                </div>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=180"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=180"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+            <li class="pagelist wlist2">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=183"><span class="icon icon-workspace"></span></a>
+                </div>
+
+                <h2 class="plname wlname"><a href="/works_show.aspx?id=183">家具</a></h2>
+                <div class="plcon wlcon"><a href="/works_show.aspx?id=183">
+                    中国目前家具零售业无外乎有四个模式：第一种模式：经销商开的独立店，青田、创典居都属于独立店，他们经营家具都有十几年的历史，在一个区域里都能开自己成型的店。第二种模式：大卖场，像红星、月星、罗浮宫的这种大卖场。招商模式的可以称为店中店。第三种模式：工厂直接开…
+                </a>
+                </div>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=183"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=183"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+            <li class="pagelist wlist3">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=182"><span class="icon icon-shirt"></span></a>
+                </div>
+
+                <h2 class="plname wlname"><a href="/works_show.aspx?id=182">美容</a></h2>
+                <div class="plcon wlcon"><a href="/works_show.aspx?id=182">
+                    利用网络可以比较准确的记录每位消费者的档案资料、产品信息、消费者喜好、满意度等等；同时，通过分析这些资料，也便于美容院的经营管理。 因此，美容院与互联网结合是未来发展的必然趋势，借助网络的力量，才能让美容院发展的更好。…
+                </a>
+                </div>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=182"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=182"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+            <li class="pagelist wlist4">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=185"><span class="icon icon-leaf"></span></a>
+                </div>
+
+                <h2 class="plname wlname"><a href="/works_show.aspx?id=185">文化</a></h2>
+                <div class="plcon wlcon"><a href="/works_show.aspx?id=185">
+                    不可否认，互联网的出现，对传统文化的传承有一定冲击，对一些文化企业的转型升级也提出了新的挑战。但互联网也同时为文化的传承和发扬提供了便捷、高效的渠道，正深刻影响着我们这个时代。 …
+                </a>
+                </div>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=185"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=185"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+            <li class="pagelist wlist5">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=186"><span class="icon icon-sister"></span></a>
+                </div>
+
+                <router-link class="industry_icon_m" style=" background-color: #f894f8;" to="/industry/Medical">
+                    <img src="/static/img/yiliao.png" alt="">
+                    <p>医疗</p>
+
+                <div class="plcon wlcon">
+                    计划经济体制下，医疗卫生体系定位明确，中国的医疗卫生创造了一系列辉煌，在医疗服务、预防保健等各个方面都取得了很大的成就。农村和城镇的医疗服务也在这时全面展开，医疗服务的可及性大大增强。由于社会经济发展和综合国力的影响以及“政事一体化”的管理，中国在医疗技术…</a>
+                </div>
+                </router-link>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=186"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=186"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+            <li class="pagelist wlist6">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=181"><span class="icon icon-cart-full"></span></a>
+                </div>
+
+                <h2 class="plname wlname"><a href="/works_show.aspx?id=181">环境</a></h2>
+                <div class="plcon wlcon"><a href="/works_show.aspx?id=181">
+                    随着互联网、大数据、云计算的快速发展，互联网已经成为社会生产、生活的基础，商业发展的重要依托。新时代背景下，如何利用互联网技术，将信息通信技术与传统行业相结合是环保领域面临的新挑战。…
+                </a>
+                </div>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=181"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=181"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+            <li class="pagelist wlist7">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=184"><span class="icon icon-plane"></span></a>
+                </div>
+
+                <h2 class="plname wlname"><a href="/works_show.aspx?id=184">地产</a></h2>
+                <div class="plcon wlcon"><a href="/works_show.aspx?id=184">
+                    随着互联网原住民成为消费主力，当汽车、商业等所有领域都已经互联网化之后，房地产也迎来了线上移民的高潮，房地产正是人口向线上移民的最后一块。意味着消费者正在往线上迁移，线上迁移之后，消费者的行为方式会发生非常大的变化。 …
+                </a>
+                </div>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=184"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=184"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+            <li class="pagelist wlist8">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=187"><span class="icon icon-balance"></span></a>
+                </div>
+
+                <h2 class="plname wlname"><a href="/works_show.aspx?id=187">旅游</a></h2>
+                <div class="plcon wlcon"><a href="/works_show.aspx?id=187">
+                    依托互联网，以满足旅游消费者信息查询、产品预定及服务评价为核心目的，囊括了包括航空公司、酒店、景区、租车公司、海内外旅游局等旅游服务供应商。…
+                </a>
+                </div>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=187"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=187"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+            <li class="pagelist wlist9">
+                <div class="plimg wlimg">
+                    <a href="/works_show.aspx?id=188"><span class="icon icon-sport-car"></span></a>
+                </div>
+
+                <h2 class="plname wlname"><a href="/works_show.aspx?id=188">政府</a></h2>
+                <div class="plcon wlcon"><a href="/works_show.aspx?id=188">
+                    电子政务是经济与社会信息化的先决条件，是指公共管理部门全面运用现代信息技术、通信技术和管理理论，将管理和服务通过网络技术进行集成，实现组织结构和业务流程的优化重组，超越时间、空间与部门分隔的限制，全方位地实施对社会的管理职能，是一种全新的管理方式，向社会提…</a>
+                </div>
+                <div class="btnmore">
+                    <a class="more animated fadeIn" href="/works_show.aspx?id=188"><span class="icon icon-detail"></span></a>
+                    <a class="on animated fadeIn" href="/works_show.aspx?id=188"><span class="icon icon-detail-on"></span></a>
+                </div>
+            </li>
+
+        </ul>
     </div>
+
 </template>
 
 <script>
@@ -16,7 +166,7 @@
 <style scoped>
     .industry_area {
         height: 318px;
-        background-color: #880000;
+        background-color: whitesmoke;
     }
     .industry_area h3 {
         font-size: 2em;
