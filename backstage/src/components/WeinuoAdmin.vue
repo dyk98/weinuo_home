ad526e7dc475e4f65cbef3de50c1b9b08ef0c6c1
Leu552 <1055112835@qq.com>
2019-01-07 16:47:52 +0800

    'feat后台更新'

---
+++ backstage/src/components/WeinuoAdmin.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="body">
+        <div>
+            <h1>Admin</h1>
+        </div>
+        <div class="fix">
+            <div class="bar_area">
+
+            </div>
+            <div class="information">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "WeinuoAdmin"
+    }
+</script>
+
+<style scoped>
+    .bar_area {
+
+    }
+</style>
