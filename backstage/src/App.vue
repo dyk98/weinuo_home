e04e98f2b4969c8fe63af5640e29eb2c030483ef
Leu552 <1055112835@qq.com>
2019-01-07 19:06:16 +0800

    'feat导航栏更新’

--- backstage/src/App.vue
+++ backstage/src/App.vue
@@ -4,25 +4,38 @@
           <h1>维诺智创后台管理系统</h1>
       </div>
       <div class="left_bar">
-          <el-collapse-transition>
-              <div class="navigation_bar" v-show="show3">
-                  <div style="flex: 2; padding: 5px 15px; background-color: #5e5e5e;">
-                      <div style="font-size: 18px; color: #FFFFFF; margin-bottom: 5px; font-weight: bold" @click="home">主页
-                      </div>
-                      <el-collapse v-model="activeName" accordion>
-                          <el-collapse-item title="服务" name="0">
-                              <div class="navigation_bar_p">软件开发</div>
-                              <div class="navigation_bar_p">交互体验</div>
-                              <div class="navigation_bar_p">运营维护</div>
-                              <div class="navigation_bar_p">资源配置</div>
-                              <div class="navigation_bar_p">质量管理</div>
-                          </el-collapse-item>
+          <div style="height: auto;" class="list">
+              <el-collapse-transition>
+                  <div v-show="show3">
 
-                      </el-collapse>
+                          <el-collapse v-model="activeNames" accordion>
+                              <el-collapse-item title="网站导航" name="1" class="section">
+                                  <div class="section_list">增</div>
+                                  <div class="section_list">删</div>
+                                  <div class="section_list">改</div>
+                                  <div class="section_list">查</div>
+
+                              </el-collapse-item>
+                              <el-collapse-item title="网站导航" name="2" class="section">
+                                  <div class="section_list">增</div>
+                                  <div class="section_list">删</div>
+                                  <div class="section_list">改</div>
+                                  <div class="section_list">查</div>
+
+                              </el-collapse-item>
+                              <el-collapse-item title="轮播图" name="3" class="section">
+                                  <div class="section_list">增</div>
+                                  <div class="section_list">删</div>
+                                  <div class="section_list">改</div>
+                                  <div class="section_list">查</div>
+
+                              </el-collapse-item>
+                          </el-collapse>
+
                   </div>
-              </div>
+              </el-collapse-transition>
+          </div>
 
-          </el-collapse-transition>
       </div>
     <router-view/>
   </div>
@@ -30,7 +43,19 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+    data() {
+        return {
+            activeNames: '0',
+            show3: true,
+        };
+    },
+    methods: {
+        handleChange(val) {
+            console.log(val);
+        }
+    }
+
 }
 </script>
 
@@ -40,15 +65,45 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+    padding: 0;
+    margin: 0;
 }
   .top_bar {
       width: auto;
       height: 20%;
-      background-color: cornflowerblue;
+      background-color: aquamarine;
   }
     .left_bar {
         height: 100%;
         width: 10em;
         background-color: darkslateblue;
+        float: top;
+    }
+.el-collapse-item__header {
+    color: #ffffff;
+    font-size: 18px;
+    background-color: darkslateblue;
+}
+
+.el-collapse-item__content {
+    padding-bottom: 2em;
+    color: white;
+    margin: 1em;
+}
+
+.el-collapse-item__wrap {
+    background-color: mediumslateblue;
+    border: none;
+    font-size: 16px;
+}
+    .list {
+        height: 100%;
+        background-color: darkslateblue;
+    }
+    .section {
+        margin: 0 auto;
+    }
+    .section_list {
+        margin: 1em auto;
     }
 </style>
