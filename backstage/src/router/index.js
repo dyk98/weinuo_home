8e188abadd2f6ad714b71096cada8ed1be86e5ce
Leu552 <1055112835@qq.com>
2019-01-08 19:09:35 +0800

    feat注册了新组件

--- backstage/src/router/index.js
+++ backstage/src/router/index.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import WeinuoAdmin from '@/components/WeinuoAdmin'
 
+import Address from '@/components/Address'
+import Connection from '@/components/Connection'
+
+import Case from '@/components/Case'
+import Move from '@/components/Move'
+
+import Industry from '@/components/Industry'
+import Program from '@/components/Program'
+import Service from '@/components/Service'
+
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +21,34 @@ export default new Router({
       path: '/',
       name: 'WeinuoAdmin',
       component: WeinuoAdmin
-    }
+    }, {
+          path: '/Address',
+          name: 'Address',
+          component: Address
+      }, {
+          path: '/Connection',
+          name: 'Connection',
+          component: Connection
+      }, {
+          path: '/Case',
+          name: 'Case',
+          component: Case
+      }, {
+          path: '/Move',
+          name: 'Move',
+          component: Move
+      }, {
+          path: '/Industry',
+          name: 'Industry',
+          component: Industry
+      }, {
+          path: '/Program',
+          name: 'Program',
+          component: Program
+      }, {
+          path: '/Service',
+          name: 'Service',
+          component: Service
+      }
   ]
 })
